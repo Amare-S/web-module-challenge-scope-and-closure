09804b3b77b8a0f2c4d8b5d78e4232c0b0b7e829
Amare Shakur <amare-shakur@lambdastudents.com>
2020-12-14 21:10:22 -0500

    completed all tests

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+    counter1 uses a closure.
   
   2. Which of the two uses a closure? How can you tell?
+   "Counter1 uses a closure, you can tell because it has a function inside of a function".
   
-  3. In what scenario would the counter1 code be preferable? In what scenario would 
+   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
 */
 
@@ -64,7 +66,9 @@ NOTE: This will be a callback function for the tasks below
 
 function inning(/*Code Here*/){
     /*Code Here*/
+    return Math.floor(Math.random() * 3);
 }
+console.log(inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,19 +85,35 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(func, inningsPlayed){
+ let final = {Home:0, Away: 0};
+ for (let i=0;i<inningsPlayed;i++){
+   final.Home+=func();
+   final.Away+=func();
+ }
+ return final;
 }
+
+ 
+
+
+
+
+
+
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inning) {
+  let Home = inning();
+  let Away = inning();
+  return {'Away':Away, 'Home':Home}
 }
 
+console.log
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
